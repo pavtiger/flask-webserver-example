7a12a041729c27d2d16cc675466d32f947255441
pavTiger <pavTiger@gmail.com>
2021-11-27 14:29:29 +0300

    Removed key press events

--- server.py
+++ server.py
@@ -29,30 +29,19 @@ def send_js(path):
     return send_from_directory('js', path)
 
 
-@socketio.on('keyup')
-def up(data):
-    username = data['user']
-    print('key pressed')
-
-
-@socketio.on('keydown')
-def down(data):
-    print('key unpressed')
-
-
 # We start a parallel thread for game logics. This loop is constantly running 
-# def game_loop(name):
-#     while True:
-#         # Process game logic here if you need to
+def game_loop(name):
+    while True:
+        # Process game logic here if you need to
 
-#         time.sleep(0.01)
+        time.sleep(0.01)
 
 
 if __name__ == "__main__":
     eventlet.monkey_patch()
 
-    # x = Thread(target=game_loop, args=(1,))
-    # x.start()
+    x = Thread(target=game_loop, args=(1,))
+    x.start()
     
     print(f'Listening on http://{ip_address}:{port}')
     socketio.run(app, host=ip_address, port=port)
